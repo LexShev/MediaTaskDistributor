a7db6e1e25e0a4a95e47918755789982cd17494d
LexShev <lexshevit@gmail.com>
2025-05-14 14:33:21 +0300

    messenger raw sql

--- planner/messenger_static/urls.py
+++ planner/messenger_static/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("<int:program_id>", index, name='messenger'),
+    path("", index, name='messenger_index'),
+    path("<int:program_id>", messenger, name='messenger'),
 
 ]
--- planner/messenger_static/views.py
+++ planner/messenger_static/views.py
@@ -1,15 +1,24 @@
-from Tools.demo.sortvisu import distinct
 from django.db.models import Count
 from django.shortcuts import render, redirect
 
 from main.permission_pannel import ask_db_permissions
 from messenger_static.forms import MessageForm
+from .messenger_utils import unique_program_id, show_messages
 from .models import Message
 
 
-def index(request, program_id):
+def index(request):
     worker_id = request.user.id
-    print(program_id)
+    # prog_count = Message.objects.values('program_id').annotate(
+    #     count=Count('program_id', distinct=True)).order_by().count()
+    data = {
+        'programs_list': unique_program_id(),
+        'permissions': ask_db_permissions(worker_id),
+    }
+    return render(request, 'messenger_static/messenger_index.html', data)
+
+def messenger(request, program_id):
+    worker_id = request.user.id
     if request.method == 'POST':
         form = MessageForm(request.POST, request.FILES)
         if form.is_valid():
@@ -21,16 +30,17 @@ def index(request, program_id):
 
     form = MessageForm()
 
-    messages = Message.objects.filter(program_id=program_id).order_by('timestamp')[:50]
-    prog_count = Message.objects.values('program_id').annotate(count=Count('program_id', distinct=True)).order_by().count()
-    prog_filter = Message.objects.filter(program_id=program_id)
-    for prog in prog_filter:
-        print(prog.program_id, prog.file)
+    # messages = Message.objects.filter(program_id=program_id).order_by('timestamp')[:50]
+    messages, viewed_messages = show_messages(program_id, worker_id)
 
 
     data = {'permissions': ask_db_permissions(worker_id),
             'messages': messages,
+            'viewed_messages': viewed_messages,
             'program_id': program_id,
+            'programs_list': unique_program_id(),
             'form': form}
 
     return render(request, 'messenger_static/messenger_index.html', data)
+
+
